49591a29ffd8fc9da4985f2ac5622e1dd53bd035
Jason Godesky <jefgodesky@gmail.com>
2020-11-08 16:18:35 -0500

    Test more demographics fetching

--- fetch.spec.js
+++ fetch.spec.js
@@ -4,8 +4,13 @@ const { fetchDemographics } = require('./fetch')
 
 describe('fetchDemographics', () => {
   it('returns something', async () => {
-    expect.assertions(1)
+    expect.assertions(4)
     const demographics = await fetchDemographics()
+    const areas = Object.keys(demographics)
+    const first = demographics[areas[0]]
     expect(demographics).toBeDefined()
+    expect(areas.length).toBeGreaterThan(0)
+    expect(Object.keys(first.byRace).length).toBeGreaterThan(0)
+    expect(Object.keys(first.byReligion).length).toBeGreaterThan(0)
   })
 })
