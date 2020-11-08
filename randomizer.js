d6d9e66d64b0ca7c2209efdc73bb0c95996281d2
Jason Godesky <jefgodesky@gmail.com>
2020-11-08 17:45:49 -0500

    randomFloatFromBellCurve

--- randomizer.js
+++ randomizer.js
@@ -8,6 +8,21 @@ const random = require('random')
 
 const randomElementFromArray = arr => arr[random.int(0, arr.length - 1)]
 
+/**
+ * Returns a value randomly selected from a normal bell curve.
+ * @param mean {number=} - The average of the normal curve. (Default: `0`)
+ * @param std {number=} - The standard deviation of the normal curve.
+ *   (Default: `1`)
+ * @returns {number} - A number randomly selected from the normal curve defined
+ *   by the mean and standard deviation provided.
+ */
+
+const randomFloatFromBellCurve = (mean = 0, std = 1) => {
+  const thunk = random.normal(mean, std)
+  return thunk()
+}
+
 module.exports = {
-  randomElementFromArray
+  randomElementFromArray,
+  randomFloatFromBellCurve
 }
--- randomizer.spec.js
+++ randomizer.spec.js
@@ -1,7 +1,8 @@
 /* global describe, it, expect */
 
 const {
-  randomElementFromArray
+  randomElementFromArray,
+  randomFloatFromBellCurve
 } = require('./randomizer')
 
 describe('randomElementFromArray', () => {
@@ -9,5 +10,11 @@ describe('randomElementFromArray', () => {
     const arr = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
     const actual = randomElementFromArray(arr)
     expect(arr.includes(actual)).toEqual(true)
+  })
+})
+
+describe('randomFloatFromBellCurve', () => {
+  it('returns a random float from a bell curve', () => {
+    expect(typeof randomFloatFromBellCurve()).toEqual('number')
   })
 })
