4ca1a8ae6fc0a1154338926647f186e4709e048e
Jason Godesky <jefgodesky@gmail.com>
2020-11-08 17:38:34 -0500

    Add documentation

--- randomizer.js
+++ randomizer.js
@@ -1,5 +1,11 @@
 const random = require('random')
 
+/**
+ * Return a random element from an array.
+ * @param arr {*[]} - An array of elements.
+ * @returns {*} - An element randomly selected from the array provided.
+ */
+
 const randomElementFromArray = arr => arr[random.int(0, arr.length - 1)]
 
 module.exports = {
