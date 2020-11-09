f456475e950d55000dc38d995db83c2b7d4f69f3
Jason Godesky <jefgodesky@gmail.com>
2020-11-09 18:56:53 -0500

    Character.addTraits

--- character.spec.js
+++ character.spec.js
@@ -119,4 +119,78 @@ describe('Character', () => {
       expect(actual.name).toEqual('Sovereign Host')
     })
   })
+
+  describe('addTraits', () => {
+    it('selects traits from the given set if you don\'t provide an existing set', () => {
+      const set1 = {
+        personality: [ 'Personality 1' ],
+        ideals: {
+          good: [ 'Good 1' ],
+          evil: [ 'Evil 1' ],
+          lawful: [ 'Lawful 1' ],
+          chaotic: [ 'Chaotic 1' ],
+          neutral: [ 'Neutral 1' ],
+          any: [ 'Any 1' ]
+        },
+        bonds: [ 'Bond 1' ],
+        flaws: [ 'Flaw 1' ]
+      }
+      const actual = Character.addTraits(null, set1, 'N')
+      const expected = {
+        personality: [ 'Personality 1' ],
+        ideals: [
+          { ideal: 'Any 1', type: 'any' },
+          { ideal: 'Neutral 1', type: 'neutral' }
+        ],
+        bonds: [ 'Bond 1' ],
+        flaws: [ 'Flaw 1' ]
+      }
+      expect(actual).toEqual(expected)
+    })
+
+    it('adds a new set of traits to an existing one', () => {
+      const set1 = {
+        personality: [ 'Personality 1' ],
+        ideals: {
+          good: [ 'Good 1' ],
+          evil: [ 'Evil 1' ],
+          lawful: [ 'Lawful 1' ],
+          chaotic: [ 'Chaotic 1' ],
+          neutral: [ 'Neutral 1' ],
+          any: [ 'Any 1' ]
+        },
+        bonds: [ 'Bond 1' ],
+        flaws: [ 'Flaw 1' ]
+      }
+      const before = Character.addTraits(null, set1, 'N')
+
+      const set2 = {
+        personality: [ 'Personality 2' ],
+        ideals: {
+          good: [ 'Good 2' ],
+          evil: [ 'Evil 2' ],
+          lawful: [ 'Lawful 2' ],
+          chaotic: [ 'Chaotic 2' ],
+          neutral: [ 'Neutral 2' ],
+          any: [ 'Any 2' ]
+        },
+        bonds: [ 'Bond 2' ],
+        flaws: [ 'Flaw 2' ]
+      }
+      const actual = Character.addTraits(before, set2, 'N')
+
+      const expected = {
+        personality: [ 'Personality 1', 'Personality 2' ],
+        ideals: [
+          { ideal: 'Any 1', type: 'any' },
+          { ideal: 'Neutral 1', type: 'neutral' },
+          { ideal: 'Any 2', type: 'any' },
+          { ideal: 'Neutral 2', type: 'neutral' }
+        ],
+        bonds: [ 'Bond 1', 'Bond 2' ],
+        flaws: [ 'Flaw 1', 'Flaw 2' ]
+      }
+      expect(actual).toEqual(expected)
+    })
+  })
 })
