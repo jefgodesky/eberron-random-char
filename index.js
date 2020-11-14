79adc10db1a430e1f722873d74c312c077ae5fdb
Jason Godesky <jefgodesky@gmail.com>
2020-11-14 15:46:35 -0500

    Provide options to specify dragonmarks and houses

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const slugify = require('slugify')
 
 const config = require('./config.json')
+const { union } = require('./randomizer')
 const { fetchData } = require('./fetch')
 const Character = require('./character')
 
@@ -17,7 +18,9 @@ app.get('/', (req, res) => {
     areas: Object.keys(app.data.demographics),
     cultures: Object.keys(app.data.cultures).map(name => ({ name, id: `culture-${slugify(name)}` })),
     races: Object.keys(app.data.races).map(name => ({ name, id: `race-${slugify(name)}` })),
-    religions: Object.keys(app.data.religions).map(name => ({ name, id: `religion-${slugify(name)}` }))
+    religions: Object.keys(app.data.religions).map(name => ({ name, id: `religion-${slugify(name)}` })),
+    marks: union(app.data.houses.map(house => house.mark)),
+    houses: app.data.houses.map(house => house.name)
   }
   res.render('index', params)
 })
