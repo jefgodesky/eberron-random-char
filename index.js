dab5f2285a528ce94f62ca895c9bce9266cc5314
Jason Godesky <jefgodesky@gmail.com>
2021-01-20 17:03:40 -0500

    Link is per character

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/generate', (req, res) => {
   if (req.body.mark === 'Random') delete req.body.mark
   if (req.body.house === 'Random') delete req.body.house
   const characters = Character.generate(app.data, req.body.area, req.body)
-  res.render('generated', { data: app.data, characters, wiki: config.mediawiki })
+  res.render('generated', { data: app.data, characters })
 })
 
 app.listen(config.port, async () => {
